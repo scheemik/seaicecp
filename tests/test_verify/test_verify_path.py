19faa4e64ff56b938574fb54f4f2c195ebe9f09e
GreyAudron <mikhail.schee@mail.utoronto.ca>
2026-05-01 17:25:18 -0400

    test: add unit test for `verify_path()`

---
+++ tests/test_verify/test_verify_path.py
@@ -0,0 +1,44 @@
+import os
+
+from seaicecp import verify
+
+def test_verify_path():
+    """Test the verify_path function."""
+    # Define list of valid paths to test
+    valid_paths = [
+        'README.md',
+        'pyproject.toml',
+        'src/seaicecp/',
+    ]
+    for valid_path in valid_paths:
+        actual = verify.verify_path(valid_path)
+        assert type(verify.verify_path(valid_path)) == type('str'), f"verify_path failed on valid path: {valid_path}"
+
+    # Define a list of invalid paths to test
+    invalid_paths = [
+        'invalid/path/to/file.txt',
+        'invalid/directory/',
+    ]
+    for invalid_path in invalid_paths:
+        try:
+            verify.verify_path(invalid_path)
+        except (FileNotFoundError) as e:
+            assert True, f"verify_path raised an exception on invalid path: {e}"
+        else:
+            assert False, f"verify_path did not raise an exception on invalid path {invalid_path}"
+    
+    # Define a list of paths with invalid types to test
+    invalid_paths = [
+        1234,
+        3.14,
+        None,
+        [],
+        {}
+    ]
+    for invalid_path in invalid_paths:
+        try:
+            verify.verify_path(invalid_path)
+        except (TypeError) as e:
+            assert True, f"verify_path raised an exception on invalid path: {e}"
+        else:
+            assert False, f"verify_path did not raise an exception on invalid path {invalid_path}"
