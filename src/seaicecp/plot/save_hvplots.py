805d755aa5e9f3effc44a175092c82077ee828be
GreyAudron <mikhail.schee@mail.utoronto.ca>
2026-05-05 11:19:15 -0400

    feat: create default `.png` filename, assume files in `outputs/` directory

--- src/seaicecp/plot/save_hvplots.py
+++ src/seaicecp/plot/save_hvplots.py
@@ -1,10 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import holoviews as hv
+from seaicecp.verify import verify_path
 
 def html_to_png(
-    html_filepath: str, 
-    png_filepath: str = None,
+    html_filename: str, 
+    png_filename: str = None,
 ):
     """ Save an `html` `hvplot` as a `png`.
 
@@ -12,10 +13,10 @@ def html_to_png(
 
         Parameters
         ----------
-        html_filepath : `str`
-            The file path to the `html` file of the `hvplot`.
-        png_filepath : `str`, `None`, optional
-            The file path to which to save the `png` image, or `None`. 
+        html_filename : `str`
+            The file path within the `outputs/` directory to the `html` file of the `hvplot`.
+        png_filename : `str`, `None`, optional
+            The file path within the `outputs/` directory to which to save the `png` image, or `None`. 
 
         Returns
         -------
@@ -26,10 +27,21 @@ def html_to_png(
         >>> 
     """
     # Verify input arguments
-    if not isinstance(html_filepath, str):
-        raise TypeError(f"(html_to_png) `html_filepath` must be a string. Got type: {type(html_filepath)}")
-    if not isinstance(png_filepath, (str, type(None))):
-        raise TypeError(f"(html_to_png) `png_filepath` must be a string. Got type: {type(png_filepath)}")
+    if not isinstance(html_filename, str):
+        raise TypeError(f"(html_to_png) `html_filename` must be a string. Got type: {type(html_filename)}")
+    html_filename = verify_path(f"outputs/{html_filename}")
+    if '.html' not in html_filename:
+        raise ValueError(f"(html_to_png) `html_filename` must be an `html` filepath. Got: {html_filename}")
+    if not isinstance(png_filename, (str, type(None))):
+        raise TypeError(f"(html_to_png) `png_filename` must be a string or `None`. Got type: {type(png_filename)}")
+    if isinstance(png_filename, str):
+        if '.png' not in png_filename:
+            raise ValueError(f"(html_to_png) `png_filename` must be a `png` filepath. Got: {png_filename}")
+        # Prepend to put it in the `outputs/` directory
+        png_filename = f"outputs/{png_filename}"
+    if isinstance(png_filename, type(None)):
+        # Define the new path to be the same as `html_filename`, but with the `.png` extension
+        png_filename = html_filename.replace('.html', '.png')
 
     # Activate the `bokeh` extension for HoloViews
     hv.extension("bokeh")
@@ -42,8 +54,8 @@ def html_to_png(
     # Start the chromium webdriver with the above options
     driver = webdriver.Chrome(options=options) 
     # Load the specified `html` plot
-    driver.get("file://" + f"/workspace/{html_filepath}")
+    driver.get("file://" + f"/workspace/{html_filename}")
     # Save a screenshot of that plot as a `png`
-    driver.save_screenshot("plot3.png")
+    driver.save_screenshot(png_filename)
     # Stop the webdriver
     driver.quit()
