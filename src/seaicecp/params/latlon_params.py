65367e288a058299d7e10a3bbb5f8986d5a9d8a9
GreyAudron <mikhail.schee@mail.utoronto.ca>
2026-05-06 15:57:28 -0400

    feat: add module to hold latitude / longitude coordinate values

---
+++ src/seaicecp/params/latlon_params.py
@@ -0,0 +1,8 @@
+# Define coordinate values for particular places
+
+# Northwest Passage Region
+# "The Northwest Passage (NWP) region covers the area between 170°W–80°W and 65°N–77°N, ..." Saenko et al. 2025 (on page 2)
+NWP_LON_MIN = -170
+NWP_LON_MAX = -80
+NWP_LAT_MIN = 65
+NWP_LAT_MAX = 77
