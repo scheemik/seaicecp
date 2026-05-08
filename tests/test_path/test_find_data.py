ddf97b4b73baf63d0dfda221aee871954646c61c
GreyAudron <mikhail.schee@mail.utoronto.ca>
2026-05-08 13:58:50 -0400

    test: add unit test for `get_model_path()`

--- tests/test_path/test_find_data.py
+++ tests/test_path/test_find_data.py
@@ -117,3 +117,141 @@ def test_list_available_models():
                 assert True, f"`list_available_models` raised an exception on invalid `institution_id`: {e}"
             else:
                 assert False, f"`list_available_models` did not raise an exception on invalid `institution_id` {invalid_string}"
+
+def test_get_model_path():
+    """Test the `get_model_path` function."""
+    # Define test cases
+    ## Note: The expected output of these test cases is manually kept up to date
+    test_cases = [
+        {
+            'source_id': 'AWI-CM-1-1-HR',
+            'expected_path': '/seaicecp_data/bergybits/data/CMIP6/HighResMIP/AWI/AWI-CM-1-1-HR'
+        },
+        {
+            'source_id': 'AWI-CM-1-1-LR',
+            'expected_path': '/seaicecp_data/bergybits/data/CMIP6/HighResMIP/AWI/AWI-CM-1-1-LR'
+        },
+        {
+            'source_id': 'BCC-CSM2-HR',
+            'expected_path': '/seaicecp_data/bergybits/data/CMIP6/HighResMIP/BCC/BCC-CSM2-HR'
+        },
+        {
+            'source_id': 'CESM1-CAM5-SE-HR',
+            'expected_path': '/seaicecp_data/bergybits/data/CMIP6/HighResMIP/NCAR/CESM1-CAM5-SE-HR'
+        },
+        {
+            'source_id': 'CESM1-CAM5-SE-LR',
+            'expected_path': '/seaicecp_data/bergybits/data/CMIP6/HighResMIP/NCAR/CESM1-CAM5-SE-LR'
+        },
+        {
+            'source_id': 'EC-Earth3P',
+            'expected_path': '/seaicecp_data/bergybits/data/CMIP6/HighResMIP/EC-Earth-Consortium/EC-Earth3P'
+
+        },
+        {
+            'source_id': 'EC-Earth3P-HR',
+            'expected_path': '/seaicecp_data/bergybits/data/CMIP6/HighResMIP/EC-Earth-Consortium/EC-Earth3P-HR'
+
+        },
+        {
+            'source_id': 'HadGEM3-GC31-HM',
+            'expected_path': '/seaicecp_data/bergybits/data/CMIP6/HighResMIP/MOHC/HadGEM3-GC31-HM'
+        },
+        {
+            'source_id': 'HadGEM3-GC31-LL',
+            'expected_path': '/seaicecp_data/bergybits/data/CMIP6/HighResMIP/MOHC/HadGEM3-GC31-LL'
+        },
+        {
+            'source_id': 'HadGEM3-GC31-MM',
+            'expected_path': '/seaicecp_data/bergybits/data/CMIP6/HighResMIP/MOHC/HadGEM3-GC31-MM'
+        },
+    ]
+    for test_case in test_cases:
+        actual = path.get_model_path(source_id=test_case['source_id'])
+        assert actual == test_case['expected_path'], f"`get_model_path` failed on test case: {test_case}."
+
+    # Define invalid test cases
+    invalid_test_cases = [
+        {
+            'source_id': 'invalid_string',
+            'data_dir': '/seaicecp_data/bergybits/data',
+            'project': 'CMIP6',
+            'activity_id': 'HighResMIP',
+        },
+        {
+            'source_id': 'HadGEM3-GC31-HM',
+            'data_dir': 'invalid_string',
+            'project': 'CMIP6',
+            'activity_id': 'HighResMIP',
+        },
+        {
+            'source_id': 'HadGEM3-GC31-HM',
+            'data_dir': '/seaicecp_data/bergybits/data',
+            'project': 'invalid_string',
+            'activity_id': 'HighResMIP',
+        },
+        {
+            'source_id': 'HadGEM3-GC31-HM',
+            'data_dir': '/seaicecp_data/bergybits/data',
+            'project': 'CMIP6',
+            'activity_id': 'invalid_string',
+        },
+    ]
+    for invalid_test_case in invalid_test_cases:
+        try:
+            actual = path.get_model_path(
+                source_id = invalid_test_case['source_id'],
+                data_dir = invalid_test_case['data_dir'],
+                project = invalid_test_case['project'],
+                activity_id = invalid_test_case['activity_id'],
+            )
+        except (FileNotFoundError) as e:
+            assert True, f"`get_model_path` raised an exception on invalid test case: {e}"
+        else:
+            assert False, f"`get_model_path` did not raise an exception on invalid test case {invalid_test_case}"
+    
+    # Define a list of invalid strings
+    invalid_strings = [
+        1234,
+        3.14,
+        None,
+        [],
+        {}
+    ]
+    for invalid_string in invalid_strings:
+        # Test with `source_id`
+        try:
+            actual = path.get_model_path(
+                source_id = invalid_string,
+            )
+        except (TypeError) as e:
+            assert True, f"`get_model_path` raised an exception on invalid `source_id`: {e}"
+        else:
+            assert False, f"`get_model_path` did not raise an exception on invalid `source_id` {invalid_string}"
+        # Test with `data_dir`
+        try:
+            actual = path.get_model_path(
+                data_dir = invalid_string,
+            )
+        except (TypeError) as e:
+            assert True, f"`get_model_path` raised an exception on invalid `data_dir`: {e}"
+        else:
+            assert False, f"`get_model_path` did not raise an exception on invalid `data_dir` {invalid_string}"
+        # Test with `project`
+        try:
+            actual = path.get_model_path(
+                project = invalid_string,
+            )
+        except (TypeError) as e:
+            assert True, f"`get_model_path` raised an exception on invalid `project`: {e}"
+        else:
+            assert False, f"`get_model_path` did not raise an exception on invalid `project` {invalid_string}"
+        # Test with `activity_id`
+        try:
+            actual = path.get_model_path(
+                activity_id = invalid_string,
+            )
+        except (TypeError) as e:
+            assert True, f"`get_model_path` raised an exception on invalid `activity_id`: {e}"
+        else:
+            assert False, f"`get_model_path` did not raise an exception on invalid `activity_id` {invalid_string}"
