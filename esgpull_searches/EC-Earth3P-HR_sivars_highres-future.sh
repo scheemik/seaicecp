dac5cd0a40751afd6e0ff7d621a3bbfe4ab153ae
GreyAudron <mikhail.schee@mail.utoronto.ca>
2026-05-12 10:50:30 -0400

    feat: add `esgpull` search for EC-Earth3P-HR `highres-future` experiment for sea ice variables

---
+++ esgpull_searches/EC-Earth3P-HR_sivars_highres-future.sh
@@ -0,0 +1,9 @@
+uv run esgpull search \
+project:CMIP6 \
+activity_id:HighResMIP \
+variable_id:areacello \
+experiment_id:highres-future \
+source_id:EC-Earth3P-HR \
+frequency:mon \
+variable_id:siage,siconc,sithick,siu,siv
+# Expect to find 25 datasets.
