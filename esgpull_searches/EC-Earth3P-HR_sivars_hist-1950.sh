3c7d0ea1c1434339c95ca1cf9099ef92e5745682
GreyAudron <mikhail.schee@mail.utoronto.ca>
2026-05-12 09:59:06 -0400

    feat: include `esgpull` search commands used to download data so far

---
+++ esgpull_searches/EC-Earth3P-HR_sivars_hist-1950.sh
@@ -0,0 +1,9 @@
+uv run esgpull search \
+project:CMIP6 \
+activity_id:HighResMIP \
+variable_id:areacello \
+experiment_id:hist-1950 \
+source_id:EC-Earth3P-HR \
+frequency:mon \
+variable_id:siage,siconc,sithick,siu,siv
+# Expect to find 30 datasets.
---
+++ esgpull_searches/HRMIP_areacello.sh
@@ -0,0 +1,5 @@
+uv run esgpull search \
+project:CMIP6 \
+activity_id:HighResMIP \
+variable_id:areacello
+# Expect to find 51 datasets.
