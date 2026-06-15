78dd1ea6ac051a2918691848d5e074dce6aadf0f
GreyAudron <mikhail.schee@mail.utoronto.ca>
2026-06-15 14:40:22 -0700

    data: add `sispeed` to the sea ice variables of EC-Earth3P-HR

--- esgpull_searches/EC-Earth3P-HR_sivars_hist-1950.sh
+++ esgpull_searches/EC-Earth3P-HR_sivars_hist-1950.sh
@@ -5,6 +5,6 @@ data_node:eagle.alcf.anl.gov,esgf-node.ornl.gov \
 experiment_id:hist-1950 \
 source_id:EC-Earth3P-HR \
 frequency:mon \
-variable_id:siage,siconc,sithick,siu,siv
-# Expect to find 30 datasets.
-# Downloads 975 files (23.9 GiB)
+variable_id:siage,siconc,sispeed,sithick,siu,siv
+# Expect to find 36 datasets.
+# Downloads 975 files (28.7 GiB)
