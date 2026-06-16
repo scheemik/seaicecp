ba004576ec7f25a3dc5eb66b72da5e2512b1e1d7
GreyAudron <mikhail.schee@mail.utoronto.ca>
2026-06-16 17:26:40 -0500

    test: add unit test of `find_slow_ice()`

--- tests/test_analysis/test_landfast.py
+++ tests/test_analysis/test_landfast.py
@@ -116,3 +116,116 @@ def test_find_packed_ice():
             assert True, f"`find_packed_ice` raised an exception on invalid `packed_threshold`: {e}"
         else:
             assert False, f"`find_packed_ice` did not raise an exception on invalid `packed_threshold` {invalid_threshold}"
+
+def test_find_slow_ice():
+    """Test the `find_slow_ice` function."""
+    # Create multiple example test files
+    test_file_dir = 'tests/test_analysis/example_datasets'
+    make_file_path(test_file_dir)
+    test_file_names = [
+        f"{test_file_dir}/example_dataset_0.nc",
+        f"{test_file_dir}/example_dataset_1.nc",
+        f"{test_file_dir}/example_dataset_2.nc",
+    ]
+    for test_file in test_file_names:
+        make_example_dataset(
+            n=3,
+            test_var_name='sispeed',
+            time_axis=True,
+            save_as=test_file,
+        )
+    # Define test cases
+    test_cases = [
+        {
+            'dataset': make_example_dataset(
+                n=3, 
+                test_var_name='sispeed',
+            ),
+            'slow_threshold': 3,
+            'expected_sum': 4,
+        },
+        {
+            'dataset': test_file_names,
+            'slow_threshold': 3,
+            'expected_sum': 24,
+        },
+    ]
+    for test_case in test_cases:
+        actual_dataset = analysis.find_slow_ice(
+            dataset = test_case['dataset'],
+            slow_threshold = test_case['slow_threshold'],
+        )
+        actual_sum = actual_dataset['sislow'].sum(skipna=True).values
+        assert actual_sum == test_case['expected_sum'], f"`find_slow_ice` failed on test case: {test_case}.\nExpected: {test_case['expected_sum']}\nActual: {actual_sum}"
+    # Clean up test files that were created
+    remove_non_empty_directory(test_file_dir)
+
+    # Define invalid test cases
+    invalid_test_cases = [
+        {
+            'dataset': 'invalid_dataset',
+        },
+        {
+            'dataset': make_example_dataset(
+                test_var_name='invalid_var',
+            ),
+        }
+    ]
+    for invalid_test_case in invalid_test_cases:
+        try:
+            actual = analysis.find_slow_ice(
+                dataset = invalid_test_case['dataset'],
+            )
+        except (FileNotFoundError, ValueError) as e:
+            assert True, f"`find_slow_ice` raised an exception on invalid test case: {e}"
+        else:
+            assert False, f"`find_slow_ice` did not raise an exception on invalid test case {invalid_test_case}"
+    
+    # Define a list of invalid inputs
+    invalid_strings = [
+        1234,
+        3.14,
+        None,
+        [],
+        {}
+    ]
+    for invalid_string in invalid_strings:
+        # Test with `dataset`
+        try:
+            actual = analysis.find_slow_ice(
+                dataset = invalid_string,
+            )
+        except (TypeError, ValueError) as e:
+            assert True, f"`find_slow_ice` raised an exception on invalid `dataset`: {e}"
+        else:
+            assert False, f"`find_slow_ice` did not raise an exception on invalid `dataset` {invalid_string}"
+        # Test with `save_as`
+        if not isinstance(invalid_string, type(None)):
+            try:
+                actual = analysis.find_slow_ice(
+                    dataset = test_cases[0]['dataset'],
+                    save_as = invalid_string,
+                )
+            except (TypeError) as e:
+                assert True, f"`find_slow_ice` raised an exception on invalid `save_as`: {e}"
+            else:
+                assert False, f"`find_slow_ice` did not raise an exception on invalid `save_as` {invalid_string}"
+    # Define a list of invalid thresholds
+    invalid_thresholds = [
+        '1234',
+        '3.14',
+        None,
+        [],
+        {}
+    ]
+    for invalid_threshold in invalid_thresholds:
+        # Test with `slow_threshold`
+        try:
+            actual = analysis.find_slow_ice(
+                dataset = test_cases[0]['dataset'],
+                slow_threshold = invalid_threshold
+            )
+        except (TypeError, ValueError) as e:
+            assert True, f"`find_slow_ice` raised an exception on invalid `slow_threshold`: {e}"
+        else:
+            assert False, f"`find_slow_ice` did not raise an exception on invalid `slow_threshold` {invalid_threshold}"
