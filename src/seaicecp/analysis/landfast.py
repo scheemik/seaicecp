0daa304c0f06100dc3cc99e08c8dd1088b58095e
GreyAudron <mikhail.schee@mail.utoronto.ca>
2026-06-17 08:56:35 -0500

    feat: add function to mark landfast ice by combining `find_packed_ice()` and `find_slow_ice()`

--- src/seaicecp/analysis/landfast.py
+++ src/seaicecp/analysis/landfast.py
@@ -274,3 +274,102 @@ def find_slow_ice(
 
     # Rename `sispeed` in the new dataset to `sislow`
     return slowice_xr.rename_vars({'sispeed':'sislow'})
+
+def find_landfast_ice(
+    siconc_dataset: (str, [str], xr.DataArray, xr.Dataset),
+    sispeed_dataset: (str, [str], xr.DataArray, xr.Dataset),
+    save_as: str = None,
+    verbose: bool = False,
+    **kwargs,
+):
+    """ Calculate where landfast ice is from the dataset(s).
+
+        Verify the dataset(s) contains the `siconc` and `sispeed` variables, calculates `sipacked` and `sislow` using `find_packed_ice()` and `find_landfast_ice()`, then takes the overlap of these to define `silandfast`.
+
+        Parameters
+        ----------
+        siconc_dataset : `str`, list of `str`, `xarray.DataArray`, `xarray.Dataset`
+            The dataset of which to find the locations of landfast ice that contains `siconc`.
+        sispeed_dataset : `str`, list of `str`, `xarray.DataArray`, `xarray.Dataset`
+            The dataset of which to find the locations of landfast ice that contains `sispeed`.
+        save_as : `str`, `None`, optional
+            The file name to pass to `cdo.setrtoc2(output=save_as)`.
+            Default is `None`, which doesn't save the dataset to a file.
+        verbose : `bool`, optional
+            Whether to verbosely output information as the function executes.
+            Default is `False`.
+        **kwargs
+            Keyword arguments to pass to `find_packed_ice()`, `find_slow_ice()` and `cdo.setrtoc2()`.
+
+        Returns
+        -------
+        landfastice_xr : `xarray.Dataset`
+            A dataset where landfast ice is marked as `1` and all other values are `0`.
+        
+        Examples
+        --------
+        >>> from seaicecp.dataset.example_dataset import make_example_dataset
+        >>> dataset_0 = make_example_dataset(n=3, test_var_name='siconc')
+        >>> dataset_0['siconc'].values
+        array([[0., 1., 2.],
+               [3., 4., 5.],
+               [6., 7., 8.]])
+        >>> dataset_1 = make_example_dataset(n=3, test_var_name='sispeed')
+        >>> dataset_1['sispeed'].values
+        array([[0., 1., 2.],
+               [3., 4., 5.],
+               [6., 7., 8.]])
+        >>> from seaicecp.analysis.landfast import find_landfast_ice
+        >>> dataset_landfast = find_landfast_ice(siconc_dataset=dataset_0, sispeed_dataset=dataset_1, packed_threshold=4, slow_threshold=4)
+        >>> dataset_landfast['silandfast'].values
+        array([[0., 0., 0.],
+               [0., 1., 0.],
+               [0., 0., 0.]])
+    """
+    # Verify input arguments
+    ## `siconc_dataset` and `sispeed_dataset` are verified by `find_packed_ice()` and `find_slow_ice()`, respectively
+    if not isinstance(save_as, (str, type(None))):
+        raise TypeError(f"(find_landfast_ice) `save_as` must be a string or `None`. Got type: {type(save_as)}")
+    elif isinstance(save_as, str) and not '.nc' in save_as:
+        raise TypeError(f"(find_landfast_ice) `save_as` must be a `.nc` filepath. Got: {save_as}")
+    
+    # Information to output
+    if verbose:
+        print(f"(find_landfast_ice) `save_as`: {save_as}")
+
+    # Find the packed and slow ice
+    dataset_sipacked = find_packed_ice(
+        dataset=siconc_dataset,
+        **kwargs,
+    )
+    dataset_sislow = find_slow_ice(
+        dataset=sispeed_dataset,
+        **kwargs,
+    )
+
+    # Check to make sure they are the same size data set
+    if dataset_sipacked.sizes != dataset_sislow.sizes:
+        raise ValueError(f"(find_landfast_ice) `siconc_dataset` and `sispeed_dataset` must have the same dimension sizes.\n`siconc_dataset.sizes`: {siconc_dataset.sizes}\n`sispeed_dataset.sizes`: {sispeed_dataset.sizes}")
+
+    # Combine these datasets
+    dataset_sipacked['sipacked'] = dataset_sipacked['sipacked'] + dataset_sislow['sislow']
+
+    # Assemble the string to specify the range and the output values
+    range_min = 1.5
+    range_max = 2.5
+    val_inside_range = 1
+    val_outside_range = 0
+    range_string = f"{range_min},{range_max},{val_inside_range},{val_outside_range}"
+
+    # Create a new dataset for `silandfast`, landfast ice
+    if verbose:
+        print(f"(find_landfast_ice) `input_command`: cdo setrtoc2,{range_string} dataset")
+    # If only processing one `xr.Dataset`, the `input` argument cannot include the range string
+    landfastice_xr = cdo.setrtoc2(
+        range_string,
+        input=dataset_sipacked, 
+        returnXDataset='silandfast'
+    )
+
+    # Rename `sipacked` in the new dataset to `silandfast`
+    return landfastice_xr.rename_vars({'sipacked':'silandfast'})
